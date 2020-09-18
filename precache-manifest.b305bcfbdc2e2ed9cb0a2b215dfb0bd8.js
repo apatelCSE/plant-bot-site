b8ecb85438d31a1e07bbd4d5140444c11a40efeb
Anika <anikapatel120@gmail.com>
2020-09-18 11:29:31 -0700

    Updates

---
+++ precache-manifest.b305bcfbdc2e2ed9cb0a2b215dfb0bd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e005e95f36d61d0b2b11a91723709b",
+    "url": "/plant-bot-site/index.html"
+  },
+  {
+    "revision": "037b751dc799e62ec20c",
+    "url": "/plant-bot-site/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "79ba31fa1d985c858ffc",
+    "url": "/plant-bot-site/static/css/main.6a4556ae.chunk.css"
+  },
+  {
+    "revision": "037b751dc799e62ec20c",
+    "url": "/plant-bot-site/static/js/2.b23deb3e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plant-bot-site/static/js/2.b23deb3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ba31fa1d985c858ffc",
+    "url": "/plant-bot-site/static/js/main.d9cf85cf.chunk.js"
+  },
+  {
+    "revision": "5d0a99e06ac2860924f8",
+    "url": "/plant-bot-site/static/js/runtime-main.b6a87670.js"
+  },
+  {
+    "revision": "02ea2012033e68e6a6395bb50dde94d1",
+    "url": "/plant-bot-site/static/media/plant.02ea2012.svg"
+  }
+]);
